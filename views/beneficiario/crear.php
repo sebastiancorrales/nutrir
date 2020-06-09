d1c2302b0e83bde09a33d4b54169e981dd4f342a
sebastiancorrales <sebastiancorrales477@gmail.com>
2020-06-09 11:06:41 -0500

    Formulario de beneficiario 2

--- views/beneficiario/crear.php
+++ views/beneficiario/crear.php
@@ -1,6 +1,6 @@
 <section class="datos-generales">
     <div class="form">
-    <h1>Datos Generales</h1>
+        <h1>Datos Generales</h1>
         <form action="" method="POST">
 
             <input type="hidden" name="datos_generales" value="datos_generales">
@@ -33,6 +33,7 @@
         </form>
     </div>
 </section>
+
 <section class="datos-institucionales">
     <h1>Datos Institucionales</h1>
     <div class="form">
@@ -48,7 +49,7 @@
             No<input type="radio" name="parentesco_familiar" value="NO">
         </div>
         <input type="text" name="cual_parentesco" placeholder="¿Cuál?">
-        
+
         <!-- Tipologia Familiar -->
         <select name="tipologia_familiar" id="tipologia_familiar">
             <option value="compuesta">Compuesta</option>
@@ -57,11 +58,158 @@
 
         <div>
             <label for="parentesco">Población Desplazada</label>
-            <input type="radio" name="poblacion_desplazada" value="SI">
-            <input type="radio" name="poblacion_desplazada" value="NO">
+            Si<input type="radio" name="poblacion_desplazada" value="SI">
+            No<input type="radio" name="poblacion_desplazada" value="NO">
+        </div>
+        <div>
+            <label for="parentesco">¿Victima de Otro tipo de Violencia</label>
+            Si<input type="radio" name="victima_otro_tipo_violencia" value="SI">
+            No<input type="radio" name="victima_otro_tipo_violencia" value="NO">
         </div>
 
+        <input type="number" name="peso_ingreso" placeholder="Peso al Ingreso">
+        <input type="number" name="peso_ingreso" placeholder="Talla al Ingreso">
 
-
+        <textarea name="estado_nutricional" id="estado_nutricional" cols="30" rows="5"
+            placeholder="Estado nutricional del beneficiario"></textarea>
+        <label for="acepta_uso_informacion">Acepta el uso y manejo de la informacion</label>
+        <input type="checkbox" name="acepta_uso_informacion" id="acepta_uso_informacion" value="SI">
     </div>
+</section>
+
+<section class="datos_poblacionales">
+    <h1>Datos Poblacionales</h1>
+    <label for="pertenencia_etnica">Pertenecia Étnica</label>
+    <select name="pertenencia_etnica" id="pertenencia_etnica">
+        <option value="">Sele</option>
+    </select>
+
+    <label for="grupo_etario">grupo Etario</label>
+    <select name="grupo_etario" id="grupo_etario">
+        <option value="">Sele</option>
+    </select>
+
+    <label for="tipo_poblacion">Tipo Población</label>
+    <select name="tipo_poblacion" id="tipo_poblacion">
+        <option value="">Sele</option>
+    </select>
+
+</section>
+
+<section class="estructura_familiar">
+    <h1>Estructura Familiar</h1>
+    <div>
+        <label for="">Nombre Completo</label>
+        <input type="text" value="nombre_estructura">
+        <label for="parentesco_estructura">Parentesco Familiar</label>
+        <select name="parentesco_estructura" id="parentesco_estructura">
+            <option value="">Sele</option>
+        </select>
+        <input type="number" name="edad_parentesco" placeholder="Edad">
+    </div>
+</section>
+
+<section class="programas_sociales">
+    <h1>Programas Sociales</h1>
+
+    <label for="">¿Inscrito en otro programa social?</label>
+    <select name="iscrito_otro_programa_social" id="iscrito_otro_programa_social">
+        <option value="SI">Si</option>
+        <option value="NO">No</option>
+    </select>
+    <input type="text" name="programa_social_inscrito" placeholder="¿Qué programa social?">
+
+    <label for="">¿Algún tipo de subsidio?</label>
+    <select name="iscrito_otro_programa_social" id="iscrito_otro_programa_social">
+        <option value="SI">Si</option>
+        <option value="NO">No</option>
+    </select>
+    <input type="text" name="que_tipo_subsidio" placeholder="¿Algún tipo de subsidio?">
+    <input type="text" name="ingresos_recibidos" placeholder="$ Ingresos recibidos">
+</section>
+
+<section class="educacion">
+    <h1>Educación</h1>
+
+    <label for="sabe_leer_escribir">¿Sabe leer y escribir?</label>
+    <select name="sabe_leer_escribir" id="sabe_leer_escribir">
+        <option value="SI">Si</option>
+        <option value="NO">No</option>
+    </select>
+
+    <label for="nivel_educativo">¿Nivel Educativo?</label>
+    <select name="nivel_educativo" id="nivel_educativo">
+        <option value="">Sele</option>
+    </select>
+
+    <label for="estidia_actualmente">¿Estudia Actualmente?</label>
+    <select name="estidia_actualmente" id="estidia_actualmente">
+        <option value="SI">Si</option>
+        <option value="NO">No</option>
+    </select>
+
+    <label for="estidia_actualmente">Grado que cursa actualmente</label>
+    <select name="estidia_actualmente" id="estidia_actualmente">
+        <option value="1">1°</option>
+        <option value="2">2°</option>
+        <option value="3">3°</option>
+        <option value="4">4°</option>
+        <option value="5">5°</option>
+        <option value="6">6°</option>
+        <option value="7">7°</option>
+        <option value="8">8°</option>
+        <option value="9">9°</option>
+        <option value="10">10°</option>
+        <option value="11">11°</option>
+    </select>
+
+    <label for="jornada_estudio">Jornada de estudio</label>
+    <select name="jornada_estudio" id="jornada_estudio">
+        <option value="mañana">mañana</option>
+        <option value="tarde">tarde</option>
+        <option value="noche">noche</option>
+    </select>
+
+    <label for="realiza_algun_curso">Realiza algún curso actualmente</label>
+    <select name="realiza_algun_curso" id="realiza_algun_curso">
+        <option value="SI">Si</option>
+        <option value="NO">No</option>
+    </select>
+    <input type="text" name="que_curso_realiza" placeholder="¿Cuál?">
+</section>
+
+<section class="seguridad-social">
+    <h1>Seguridad Social</h1>
+
+    <input type="text" name="nombre_eps" placeholder="Nombre de la EPS">
+
+    <label for="tipo_afiliacion">Tipo de Afiliación</label>
+    <select name="tipo_afiliacion" id="tipo_afiliacion">
+        <option value="subsidiado">Subsidiado</option>
+        <option value="contributivo">contributivo</option>
+    </select>
+
+    <label for="tiene_sisben">Tiene Sisben</label>
+    <select name="tiene_sisben" id="tiene_sisben">
+        <option value="SI">Si</option>
+        <option value="NO">No</option>
+    </select>
+
+    <label for="puntaje_sisben">Puntaje Sisben</label>
+    <select name="puntaje_sisben" id="puntaje_sisben">
+        <option value="1">1</option>
+        <option value="2">2</option>
+        <option value="3">3</option>
+        <option value="4">4</option>
+    </select>
+
+    
+    <label for="tipo_afiliacion">Regimen de Seguridad Social</label>
+    <select name="tipo_afiliacion" id="tipo_afiliacion">
+        <option value="subsidiado">Subsidiado</option>
+        <option value="contributivo">contributivo</option>
+    </select>
+
+
+
 </section>
